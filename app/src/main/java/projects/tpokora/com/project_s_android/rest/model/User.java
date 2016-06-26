ffceb4d8abfb647f279775cafc4ecc0621448abc
tpokora <pokora.tomek@gmail.com>
2016-06-26 12:10:40 +0200

    User model

---
+++ app/src/main/java/projects/tpokora/com/project_s_android/rest/model/User.java
@@ -0,0 +1,9 @@
+package projects.tpokora.com.project_s_android.rest.model;
+
+/**
+ * Created by pokor on 24.06.2016.
+ */
+public class User {
+    private String login;
+    private String password;
+}
