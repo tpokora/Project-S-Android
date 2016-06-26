9e07867c346685bec506381647192847a713260d
tpokora <pokora.tomek@gmail.com>
2016-06-26 13:34:44 +0200

    New Article button

--- app/src/main/java/projects/tpokora/com/project_s_android/ArticleActivity.java
+++ app/src/main/java/projects/tpokora/com/project_s_android/ArticleActivity.java
@@ -3,7 +3,10 @@ package projects.tpokora.com.project_s_android;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by pokor on 20.06.2016.
@@ -17,6 +20,7 @@ public class ArticleActivity extends AppCompatActivity {
     private Context context;
 
     private TextView loggedUserBar;
+    private Button newArticleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,14 @@ public class ArticleActivity extends AppCompatActivity {
         // Initiate components
         loggedUserBar = (TextView) findViewById(R.id.article_user_bar);
         loggedUserBar.setText(loggedUser);
+
+        newArticleButton = (Button) findViewById(R.id.new_article_button);
+        newArticleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "This will open new article menu", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void getBundle() {
