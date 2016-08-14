37121876bb51d5bf3a9825cec33856346e78d289
tpokora <pokora.tomek@gmail.com>
2016-08-14 17:53:15 +0200

    Google Licence Notes

--- app/src/main/java/projects/tpokora/com/project_s_android/activities/AboutActivity.java
+++ app/src/main/java/projects/tpokora/com/project_s_android/activities/AboutActivity.java
@@ -1,6 +1,9 @@
 package projects.tpokora.com.project_s_android.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import projects.tpokora.com.project_s_android.R;
 
@@ -11,10 +14,15 @@ public class AboutActivity extends AbstractActivity {
 
     private static final String DEBUG_TAG = "AboutActivity";
 
+    private TextView googleLicencePolicyTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setupActivity();
+
+        googleLicencePolicyTextView = (TextView) findViewById(R.id.about_licence_notes_text);
+        googleLicencePolicyTextView.setText(GoogleApiAvailability.getInstance().getOpenSourceSoftwareLicenseInfo(context));
     }
 }
