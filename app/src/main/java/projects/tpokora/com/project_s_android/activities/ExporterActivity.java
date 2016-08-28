90959783532d44e57bcb42905f884e7249d65ef9
tpokora <pokora.tomek@gmail.com>
2016-08-28 17:05:09 +0200

    Export JSON Button layout

--- app/src/main/java/projects/tpokora/com/project_s_android/activities/ExporterActivity.java
+++ app/src/main/java/projects/tpokora/com/project_s_android/activities/ExporterActivity.java
@@ -1,6 +1,7 @@
 package projects.tpokora.com.project_s_android.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import projects.tpokora.com.project_s_android.R;
 import projects.tpokora.com.project_s_android.export.ArticlesExporter;
@@ -14,6 +15,8 @@ public class ExporterActivity extends AbstractActivity {
 
     private String loggedUser;
 
+    private Button exportJSONButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +26,12 @@ public class ExporterActivity extends AbstractActivity {
         // Get user
         loggedUser = bundle.getString("login");
 
-        articlesExporter = new ArticlesExporter(this, this.context, loggedUser);
+        //articlesExporter = new ArticlesExporter(this, this.context, loggedUser);
 
+        initUIElement();
+    }
+
+    public void initUIElement() {
+        exportJSONButton = (Button) findViewById(R.id.export_JSON_button);
     }
 }
