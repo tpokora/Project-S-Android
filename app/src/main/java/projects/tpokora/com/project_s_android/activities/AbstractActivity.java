9ffd4a722bfdaed5ea89fe7084bca8ea17cd086e
tpokora <pokora.tomek@gmail.com>
2016-09-12 20:40:27 +0200

    Add option menus to each to activity

--- app/src/main/java/projects/tpokora/com/project_s_android/activities/AbstractActivity.java
+++ app/src/main/java/projects/tpokora/com/project_s_android/activities/AbstractActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import projects.tpokora.com.project_s_android.R;
 import projects.tpokora.com.project_s_android.storage.ArticleDBAdapter;
 
 /**
@@ -20,11 +24,51 @@ public abstract class AbstractActivity extends AppCompatActivity {
     protected ArticleDBAdapter articleDBAdapter;
 
     protected Toolbar toolbar;
+    protected String loggedUser;
 
     protected void setupActivity() {
         context = this;
         bundle = getIntent().getExtras();
         activityDispatcher = new ActivityDispatcher(this, this.context);
         articleDBAdapter = new ArticleDBAdapter(this.context);
+
+        // Get user
+        if (bundle != null) {
+            loggedUser = bundle.getString("login");
+        }
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_articles, menu);
+        return true;
+    }
+
+    /**
+     * Click handlers on menu item
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings_export:
+                activityDispatcher.redirectNowWithExtras(ActivityDispatcher.EXPORTER_ACTIVITY, "login", loggedUser, true);
+                break;
+            case R.id.action_settings_articles:
+                activityDispatcher.redirectNowWithExtras(ActivityDispatcher.ARTICLES_ACTIVITY, "login", loggedUser, true);
+                break;
+            case R.id.action_settings_about:
+                activityDispatcher.redirectNow(ActivityDispatcher.ABOUT_ACTIVITY, true);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        return true;
     }
 }
