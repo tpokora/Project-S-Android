6a9397c02ed62b1d08cd09cad72eefe9f874b1a7
tpokora <pokora.tomek@gmail.com>
2016-09-13 21:02:53 +0200

    Fragments

---
+++ app/src/main/java/projects/tpokora/com/project_s_android/activities/fragments/AbstractArticlesFragment.java
@@ -0,0 +1,17 @@
+package projects.tpokora.com.project_s_android.activities.fragments;
+
+import projects.tpokora.com.project_s_android.storage.ArticleDBAdapter;
+
+/**
+ * Created by pokor on 07.09.2016.
+ */
+public class AbstractArticlesFragment extends AbstractFragment {
+
+    protected ArticleDBAdapter articleDBAdapter;
+
+    @Override
+    protected void setupFragment() {
+        super.setupFragment();
+        articleDBAdapter = new ArticleDBAdapter(this.context);
+    }
+}
