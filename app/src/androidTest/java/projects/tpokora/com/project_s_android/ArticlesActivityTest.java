c74862d9b314a06b8077394cae627ae9e205669c
tpokora <pokora.tomek@gmail.com>
2016-09-19 21:16:32 +0200

    Test - if New Article Button is clicked

--- app/src/androidTest/java/projects/tpokora/com/project_s_android/ArticlesActivityTest.java
+++ app/src/androidTest/java/projects/tpokora/com/project_s_android/ArticlesActivityTest.java
@@ -1,7 +1,6 @@
 package projects.tpokora.com.project_s_android;
 
 import android.content.Intent;
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Before;
@@ -13,6 +12,7 @@ import projects.tpokora.com.project_s_android.activities.ArticlesActivity;
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.action.ViewActions.*;
 
 /**
  * Created by pokor on 19.09.2016.
@@ -21,8 +21,6 @@ public class ArticlesActivityTest extends AbstractActivityTest {
 
     @Rule
     public ActivityTestRule<ArticlesActivity> articlesActivityActivityTestRule = new ActivityTestRule<>(ArticlesActivity.class, false, false);
-
-    private ArticlesActivity articlesActivity;
 
     @Before
     public void setupIntent() {
@@ -34,9 +32,16 @@ public class ArticlesActivityTest extends AbstractActivityTest {
     public void test_articleListFragmentIsShown_success() {
         articlesActivityActivityTestRule.launchActivity(intent);
 
-        articlesActivity = articlesActivityActivityTestRule.getActivity();
-
         // check if fragment is applied to layout
         onView(withId(R.id.fragment_article_list)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_newArticleFragmentIsShownAfterNewArticleBtnClick_success() {
+        articlesActivityActivityTestRule.launchActivity(intent);
+
+        // click on New Article button and check if NewArticleFragment is displayed
+        onView(withId(R.id.new_article_button)).perform(click());
+        onView(withId(R.id.fragment_new_article)).check(matches(isDisplayed()));
     }
 }
